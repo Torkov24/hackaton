0f496543b88f72bc775e886bbba9a0679f108340
Hristijan Torkov <73495723+Torkov24@users.noreply.github.com>
2025-05-09 14:44:09 +0200

    Update vite.config.js

--- techTitansFe/vite.config.js
+++ techTitansFe/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
-        host: '0.0.0.0',
+        host: '20.251.145.158',
         port: 3000,
     },
     base: './',
